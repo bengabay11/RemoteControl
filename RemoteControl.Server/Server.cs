c04aec71ac5012215892bc5dba12014cb9ddcd09
ben <ben.gabay38@gmail.com>
2019-03-22 00:58:01 +0200

    created basic server and server manager

---
+++ RemoteControl.Server/Server.cs
@@ -0,0 +1,29 @@
+﻿using RemoteControl.Core.Interfaces;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RemoteControl.Server
+{
+    class Server : IServer
+    {
+        private Socket _socket;
+
+        public void Bind(string ip, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Bind(localEndPoint);
+        }
+
+        public void Listen(int maxConnections)
+        {
+            _socket.Listen(maxConnections);
+        }
+
+        public Socket Accept()
+        {
+            return _socket.Accept();
+        }
+    }
+}
---
+++ RemoteControl.Server/ServerManager.cs
@@ -0,0 +1,29 @@
+﻿using RemoteControl.Core.Interfaces;
+
+namespace RemoteControl.Server
+{
+    public class ServerManager
+    {
+        private IServer _server;
+        private bool _stop;
+
+        public ServerManager(IServer server)
+        {
+            _server = server;
+            _stop = false;
+        }
+
+        public void Start()
+        {
+            while (!_stop)
+            {
+
+            }
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+        }
+    }
+}
