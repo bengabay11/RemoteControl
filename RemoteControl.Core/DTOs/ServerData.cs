2aae990a4cc0401d04c145878dc7cadc72a98de2
ben <ben.gabay38@gmail.com>
2019-03-22 10:39:43 +0200

    added ServerData DTO

---
+++ RemoteControl.Core/DTOs/ServerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControl.Core.DTOs
+{
+    public class ServerData
+    {
+        public ServerAction Action { get; set; }
+        public string Data { get; set; }
+            
+        public ServerData(ServerAction action, string data)
+        {
+            Action = action;
+            Data = data;
+        }
+    }
+}
