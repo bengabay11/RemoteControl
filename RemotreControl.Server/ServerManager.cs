79fd5fc789df2ec0567452141bdc232689c62bfc
ben <ben.gabay38@gmail.com>
2019-12-13 17:20:38 +0200

    fixed ServerService name

--- RemotreControl.Server/ServerManager.cs
+++ RemotreControl.Server/ServerManager.cs
@@ -1,6 +1,4 @@
-﻿using RemoteControl.Core.DTOs;
-using RemoteControl.Core.Interfaces;
-using System;
+﻿using RemoteControl.Core.Interfaces;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Runtime.Serialization;
@@ -10,22 +8,24 @@ namespace RemoteControl.Server
 {
     public class ServerManager : ISocketManager
     {
+        private IServer _server;
         private readonly IFormatter _formatter;
         private readonly IDictionary<ClientActionType, IClientAction> _clientActions;
         private bool _stop;
 
-        public ServerManager(IDictionary<ClientActionType, IClientAction> clientActions, IFormatter formatter)
+        public ServerManager(IServer server, IDictionary<ClientActionType, IClientAction> clientActions, IFormatter formatter)
         {
+            _server = server;
             _clientActions = clientActions;
             _formatter = formatter;
             _stop = false;
         }
 
-        private void HandleClients(IServer server)
+        private void HandleClients()
         {
             while (!_stop)
             {
-                Socket client = server.Accept();
+                Socket client = _server.Accept();
                 ClientHandler clientHandler = new ClientHandler(client, _formatter, _clientActions);
                 Task clientHandlerTask = new Task(clientHandler.Start);
                 clientHandlerTask.Start();
@@ -34,10 +34,9 @@ namespace RemoteControl.Server
 
         public void Start(string ip, int port, int maxConnections)
         {
-            IServer server = new Server();
-            server.Bind(ip, port);
-            server.Listen(maxConnections);
-            Task.Factory.StartNew(() => HandleClients(server));
+            _server.Bind(ip, port);
+            _server.Listen(maxConnections);
+            Task.Factory.StartNew(HandleClients);
         }
 
         public void Stop()
